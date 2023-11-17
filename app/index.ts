62bfb58fead2bd1f030e763e5fd71379914b89f7
matteobelin <matteobelin85@gmail.com>
2023-11-17 15:59:47 +0100

    ajout d un cours

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { Low } from "lowdb";
 import { JSONFile } from 'lowdb/node';
-import { userSchema } from './validation';
+import { courseSchema, userSchema } from './validation';
 
 
 type Data = {
   user: user[],
+  course : course[]
   }
 
 interface user{
@@ -15,12 +16,19 @@ interface user{
     role:string;
   }
 
-const defaultData: Data = { user: [] }
+  interface course{
+    id:number;
+    title:string;
+    date:string;
+    time:string;
+  }
+
+const defaultData: Data = { user: [], course: []  }
 const adapter = new JSONFile<Data>('db.json')
 const db = new Low<Data>(adapter, defaultData)
 const app: express.Application = express();
 const port: number = 3000
-db.read();
+
 
 
 
@@ -55,7 +63,27 @@ app.post("/add_user",async(req,res)=>{
     }
 });
 
+app.post("/add_course", async (req, res) => {
+    try {
+        await db.read();
 
+        if (db.data && db.data.course && Array.isArray(db.data.course)) {
+            const LastCourse = db.data.course[db.data.course.length - 1];
+            const id = LastCourse ? LastCourse.id + 1 : 1;
+            const data = await courseSchema.validate(req.body, { stripUnknown: true });
+            db.data.course.push({ id: id, ...data });
+            await db.write();
+
+            res.status(200).send("Cours ajouté");
+        } else {
+            console.error("La structure de la base de données est incorrecte ou le tableau 'course' est vide.");
+            res.status(500).send("Erreur interne du serveur");
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(400).send(err);
+    }
+});
 
 
 app.listen(port, () => {
--- app/validation.ts
+++ app/validation.ts
@@ -1,8 +1,14 @@
-import { object, string, number } from 'yup';
+import { object, string, date} from 'yup';
 
 let userSchema = object({
     email:string().email().required().trim().strict(),
     password: string().required().trim().strict(),
     role: string().required().trim().strict(),
   });
-  export{userSchema}
+
+let courseSchema=object({
+    title:string().required().trim().strict(),
+    date:string().required().strict().matches(/(\d{2})\/(\d{2})\/(\d{4})/),
+    time:string().required().matches(/([01]\d|2[0-3]):([0-5]\d)/),
+  });
+  export{userSchema,courseSchema}
